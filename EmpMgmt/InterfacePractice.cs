414d331f648a8a6206acdf9860c10799fc641101
kumar.vishal <kumar.vishal@CPU-0104>
2022-09-16 12:45:44 +0530

    Interface

---
+++ EmpMgmt/InterfacePractice.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpMgmt
+{
+    public interface IAreaCalculator
+    {
+        double GetArea();
+    }
+    public class Circle : IAreaCalculator
+    {
+        public double Radius { get; set; }
+
+        public double GetArea()
+        {
+            return Math.PI * (Radius * Radius);
+        }
+    }
+    public class Rectangle : IAreaCalculator
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double GetArea()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Triangle : IAreaCalculator
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double GetArea()
+        {
+            return Height * Width * 0.5;
+        }
+    }
+
+    public class InterfacePractice
+    {
+      
+       public void CircleArea(double rad)
+        {
+            IAreaCalculator myCalc = new Circle()
+            {
+                Radius = rad
+            };
+            PrintOutPut("Circle", Convert.ToDouble(myCalc.GetArea()));
+        }
+        public void RectangleArea(double hei,double wid)
+        {
+            IAreaCalculator myCalc = new Rectangle()
+            {
+                Height = hei,
+                Width = wid
+            };
+            PrintOutPut("Rectangle", Convert.ToDouble(myCalc.GetArea()));           
+        }
+        public void TriangleArea(double hei, double wid)
+        {
+            IAreaCalculator myCalc = new Triangle()
+            {
+                Height = hei,
+                Width = wid
+            };
+            PrintOutPut("Triangle", Convert.ToDouble(myCalc.GetArea()));
+        }
+        void PrintOutPut(string text,double Area)
+        {
+            Console.WriteLine("Area of {0} is {1}",text,Area.ToString());
+        }
+    }
+}
--- EmpMgmt/Program.cs
+++ EmpMgmt/Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmpMgmt;
 using EmpMgmt.Entity;
-using System.Drawing;
 
-Console.WriteLine("Welcome to Employee Management System!");
+Console.WriteLine($"Welcome to Employee Management System!");
 int n;
 Comman_Command_Name();
 
@@ -21,10 +20,8 @@ while (n > 0)
             Comman_Command_Name();
             break;
         case 11:
-            Console.Write("\nPlease Enter the Employee ID\t");
-            int m;
-            bool isNumericals = int.TryParse(Console.ReadLine(), out m);
-            m = isNumericals ? m : 0;
+            Console.Write($"\nPlease Enter the Employee ID\t");
+            int m = int.TryParse(Console.ReadLine(), out m) ? m : 0;
             List<Employee> lists = EmployeeMethod();
             for (int i = 0; i < lists.Count; i++)
             {
@@ -39,7 +36,7 @@ while (n > 0)
                 else if (i== lists.Count-1)
                 {
                     
-                    Console.Write("\nPlease Enter Correct Employee ID\n");
+                    Console.Write($"\nPlease Enter Correct Employee ID\n");
                     break;
                 }
 
@@ -64,15 +61,40 @@ while (n > 0)
                 {
                     if (num == 0)
                     {
-                        Console.Write("\nPlease Enter Correct Employee Name\n");
+                        Console.Write($"\nPlease Enter Correct Employee Name\n");
                         break;
                     }                    
                 }
             }
             Comman_Command_Name();
             break;
+        case 101:
+            Console.Write($"\nPlease Enter Radius:\t");
+            double rad = double.TryParse(Console.ReadLine(), out rad) ? rad : 0;
+            InterfacePractice ip = new InterfacePractice();
+            ip.CircleArea(rad);
+            Comman_Command_Name();
+            break;
+        case 102:
+            Console.Write($"\nPlease Enter Length of Rectangle:\t");
+            double len = double.TryParse(Console.ReadLine(), out len) ? len : 0;
+            Console.Write($"\nPlease Enter breadth of Rectangle:\t");
+            double br = double.TryParse(Console.ReadLine(), out br) ? br : 0;
+            InterfacePractice ipr = new InterfacePractice();
+            ipr.RectangleArea(len,br);
+            Comman_Command_Name();
+            break;
+        case 103:
+            Console.Write($"\nPlease Enter Height of Triangle:\t");
+            double He = double.TryParse(Console.ReadLine(), out He) ? He : 0;
+            Console.Write($"\nPlease Enter width of Triangle:\t");
+            double wi = double.TryParse(Console.ReadLine(), out wi) ? wi : 0;
+            InterfacePractice ipt = new InterfacePractice();
+            ipt.TriangleArea(He,wi);
+            Comman_Command_Name();
+            break;
         default:
-            Console.WriteLine("\nPlease enter correct key\n");
+            Console.WriteLine($"\nPlease enter correct key\n");
             n = 1;
             break;
     }
@@ -85,21 +107,24 @@ List<Employee> EmployeeMethod()
 }
 void Comman_Command_Name()
 {
-    Console.WriteLine("Press 1 for Show All Employee");
-    Console.WriteLine("Are you search employee by Employee ID Press 11");
-    Console.WriteLine("Are you search employee by Employee Name Press 12");
-    Console.Write("Please Enter the Number\t");
+    Console.WriteLine($"Press 1 for Show All Employee");
+    Console.WriteLine($"Are you search employee by Employee ID Press 11");
+    Console.WriteLine($"Are you search employee by Employee Name Press 12");
+    Console.WriteLine($"Calculate Area of Circle Press 101");
+    Console.WriteLine($"Calculate Area of Rectangle Press 102");
+    Console.WriteLine($"Calculate Area of Triangle Press 103");
+    Console.Write($"Please Enter the Number\t");
     bool isNumerical = int.TryParse(Console.ReadLine(), out n);
     n = isNumerical ? n : 0;
 }
 void printDetail(Employee emp)
 {
-    Console.WriteLine("ID    =" + emp.EmpID);
-    Console.WriteLine("Name  =" + emp.Name);
-    Console.WriteLine("DOB   =" + emp.DOB.ToString());
-    Console.WriteLine("Email =" + emp.EmailId);
-    Console.WriteLine("Mobile=" + emp.MobileNo);
-    Console.WriteLine("Salary =" + emp.Salary.ToString());
-    Console.WriteLine("Date of Joinning=" + emp.DOJ.ToString());
-    Console.WriteLine("");
+    Console.WriteLine($"ID    =" + emp.EmpID);
+    Console.WriteLine($"Name  =" + emp.Name);
+    Console.WriteLine($"DOB   =" + emp.DOB.ToString());
+    Console.WriteLine($"Email =" + emp.EmailId);
+    Console.WriteLine($"Mobile=" + emp.MobileNo);
+    Console.WriteLine($"Salary=₹" + emp.Salary.ToString());
+    Console.WriteLine($"Date of Joinning=" + emp.DOJ.ToString());
+    Console.WriteLine($"");
 }
